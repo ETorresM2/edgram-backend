50a88a2296de06616514498bec3d8af077c63c9b
Edwin Torres <edwin.torres.jr2@gmail.com>
2019-07-05 18:43:29 -0400

    updating message for friend

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const port = process.env.PORT || 5000;
 
 const server = express();
 
-server.get("/", (requ, res) => {
-  res.send("hello world");
+server.get("/", (req, res) => {
+  res.send("Hi Amanda, this is sent from Edwins epic server");
 });
 
 server.listen(port, () => {
