fac893c285be49b11a73db4a56220c95620223cd
Edwin Torres <edwin.torres.jr2@gmail.com>
2020-02-28 10:15:35 -0500

    uncommented dev since it didnt work

--- knexfile.js
+++ knexfile.js
@@ -3,16 +3,16 @@ const dbConnection = process.env.DATABASE_URL
 
 module.exports = {
 
-  // development: {
-  //   // our DBMS driver
-  //   client: 'sqlite3',
-  //   // the location of our db
-  //   connection: {
-  //     filename: './data/edgram.db3',
-  //   },
-  //   // necessary when using sqlite3
-  //   useNullAsDefault: true
-  // },
+  development: {
+    // our DBMS driver
+    client: 'sqlite3',
+    // the location of our db
+    connection: {
+      filename: './data/edgram.db3',
+    },
+    // necessary when using sqlite3
+    useNullAsDefault: true
+  },
 
   production: {
     client: "pg",
